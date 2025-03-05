73c1c774aab64438b28b71a7b44a1ff5823a4896
Tuthache <64441477+Tuthache@users.noreply.github.com>
2025-03-05 02:59:40 -0800

    initial commit

---
+++ frontend/src/components/searchResultsItem.js
@@ -0,0 +1,3 @@
+const searchResultsItem = () => {};
+
+export default searchResultsItem;
---
+++ frontend/src/pages/searchPage.js
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import searchResults from "../components/searchResults";
+import searchResultsFilter from "../components/searchResultsFilter";
+
+const searchPage = () => {};
+
+export default searchPage;
