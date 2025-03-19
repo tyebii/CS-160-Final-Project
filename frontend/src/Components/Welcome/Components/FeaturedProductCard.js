1e96fceabd2516e3d8164923e8321af0e4ee5ab9
tiffanyhuynh11 <tiffanyhuynh10@gmail.com>
2025-03-18 22:15:24 -0700

    carousel
    https://www.npmjs.com/package/react-multi-carousel
    run `npm install react-multi-carousel --save` in the frontend folder to install

--- frontend/src/Components/Welcome/Components/FeaturedProductCard.js
+++ frontend/src/Components/Welcome/Components/FeaturedProductCard.js
@@ -3,7 +3,7 @@ import './FeaturedProductCard.css';
 const FeaturedProductCard = ({ imageSrc, productName }) => {
   return (
     <div className="product-card">
-      <img className="product-name-img" src={imageSrc} alt={productName} />
+      <img className="product-card-img" src={imageSrc} alt={productName} />
       <p className="product-name">{productName}</p>
       <button className="add-to-cart">View Item</button>
     </div>
--- frontend/src/Components/Welcome/Welcome.js
+++ frontend/src/Components/Welcome/Welcome.js
@@ -3,27 +3,46 @@ import Carrot from './Images/carrot.png';
 import {Link } from "react-router-dom";
 import FeaturedProductCard from './Components/FeaturedProductCard';
 import CategoryCard from './Components/Category';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 function Welcome(){
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1
+        }
+    };
+
     return (
-        <div class="container">
-            <div class="welcome">
+        <div className="container">
+            <div className="welcome">
                 <h1>Welcome to OFS!</h1>
             </div>
             
-            <div class="featured-products">
-                <h2 class="section-title">Featured Products</h2>
-                <div class="products-slider">
-                    <button class="arrow">←</button>
-                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots" />
-                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots" />
-                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots" />
-                    <button class="arrow">→</button>
-                </div>
+            <div className="featured-products">
+                <h2 className="section-title">Featured Products</h2>
+                    <Carousel renderButtonGroupOutside={true} partialVisible={false} responsive={responsive} ssr={true} draggable={false} showDots={true} infinite={true} autoPlay={false} dotListClass="carousel-dots" itemClass="carousel-item-padding-40-px" containerClass="carousel-container">
+                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots1" />
+                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots2" />
+                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots3" />
+                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots4" />
+                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots5" />
+                        <FeaturedProductCard imageSrc={Carrot} productName="Carrots6" />
+                    </Carousel>
             </div>
             
-            <div class="categories">
-                <h2 class="section-title">Browse Categories</h2>
-                <div class="categories-grid">
+            <div className="welcome-categories">
+                <h2 className="section-title">Browse Categories</h2>
+                <div className="welcome-categories-grid">
                     <Link to = "/search/category/fresh-produce">
                         <CategoryCard imageSrc={Carrot} categoryName="Fresh Produce" />
                     </Link>
