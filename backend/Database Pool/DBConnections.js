f62506cacbb4d763579f5965bd5bd31ab3f91110
MatthewDel <matthewdelurio@gmail.com>
2025-03-19 12:27:23 -0700

    backend endpoints

---
+++ backend/Database Pool/DBConnections.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2') //NPM install mysql12
+
+const pool = mysql.createPool({
+    host: 'localhost',   // Use 'localhost' 
+    user: 'root', // Your MySQL username 
+    password: 'password', // Your MySQL password
+    database: 'OFS', // Your database name
+    port: 3306, // SQL Port
+  });
+  
+  // Check the connection and handle errors
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error connecting to MySQL database:', err);
+      return;
+    }
+    console.log('Connected to MySQL database');
+    connection.release(); 
+  });
+
+  module.exports = pool
---
+++ backend/ShoppingCart/ShoppingCartController.js
@@ -0,0 +1,59 @@
+//Import the database connection pool
+const pool = require('../Database Pool/DBConnections')
+
+const getShoppingCart = (req, res) => {
+    const id = req.user.customerid 
+    pool.query("select * from shoppingcart where customerid = ?", [id], (err,result)=>{
+        if(err){
+            res.status(500)
+            return;
+        }
+        res.status(200).json(result)
+        return;
+    })
+}
+
+const addToShoppingCart = (req, res) => {
+    const CustomerID = req.user.customerid 
+    const {ItemID, Quantity} = req.body
+    pool.query("INSERT INTO shoppingcart(customerid, itemid, orderquantity) VALUES (?,?,?)", [CustomerID, ItemID, Quantity], (err, result)=>{
+        if(err){
+            res.status(500);
+            return;
+        }
+        res.status(200).json({success:"true"})
+        return;
+    }
+    )
+}
+
+
+const updateShoppingCart = (req, res) => {
+    const CustomerID = req.user.customerid 
+    const {ItemID, Quantity} = req.body
+    pool.query("UPDATE shoppingcart SET orderquantity = ? WHERE customerid = ? AND itemid = ?", [Quantity, CustomerID, ItemID], (err, result)=>{
+        if(err){
+            res.status(500);
+            return;
+        }
+        res.status(200).json({success:"true"})
+        return;
+    }
+    )
+}
+
+const removeFromShoppingCart = (req, res) => {
+    const CustomerID = req.user.customerid 
+    const {ItemID} = req.body
+    pool.query("DELETE FROM shoppingcart WHERE customerid = ? AND itemid = ?", [CustomerID, ItemID], (err, result)=>{
+        if(err){
+            res.status(500);
+            return;
+        }
+        res.status(200).json({success:"true"})
+        return;
+    }
+    )
+}
+
+module.exports = {removeFromShoppingCart, updateShoppingCart, addToShoppingCart, getShoppingCart}
